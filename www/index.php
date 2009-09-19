7547484a8086d2386eee39391ee776be1a718d3e
kingaa <kingaa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-19 09:43:05 +0000

    improve the webpage
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ouch@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -14,7 +14,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<link href="./whitestyle.css" rel="stylesheet" type="text/css">
   </head>
 
 <body>
@@ -33,11 +33,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <td>
 <table cellpadding="0" cellspacing="20" border="0">
 <tr>
-<td colspan="0" rowspan="0" align="left">
-<img alt="ouch logo" src="ouch_ctr.cgi" height="79" width="52"></td>
 <td colspan="0" rowspan="0" align="right">
 <div align="right">
-<h2><font color="#E32644">OUCH</font>: <font color="#E32644">O</font>rnstein-<font color="#E32644">U</font>hlenbeck models<br> 
+<h2><font color="#E32644"><i>ouch</i></font>: <font color="#E32644">O</font>rnstein-<font color="#E32644">U</font>hlenbeck models<br> 
 for phylogenetic <font color="#E32644">C</font>omparative hypot<font color="#E32644">H</font>eses</h2>
 </div>
 </td>
@@ -46,31 +44,39 @@ for phylogenetic <font color="#E32644">C</font>omparative hypot<font color="#E32
 </td>
 <td>
 <ul>
-<li><a href="./index.php">About OUCH</a></li>
-<li><a target="_blank" href="http://cran.at.r-project.org/web/packages/ouch/ouch.pdf">OUCH Manual (PDF)</a></li>
+<li><a href="./index.php">About <i>ouch</i></a></li>
+<li><a target="_blank" href="http://cran.at.r-project.org/web/packages/ouch/ouch.pdf"><i>ouch</i> manual (PDF)</a></li>
 <li><a href="./index.php?nav=bib">References to the literature</a></li>
-<li><a href="http:<?php echo $domain; ?>/projects/<?php echo $group_name;?>">Development version of <tt>ouch</tt> (on r-forge)</a></li>
-<li><a href="http://cran.at.r-project.org/web/packages/ouch/">Release version of <tt>ouch</tt> (on CRAN)</a></li>
-<li><a href="./links.html">Related software</a></li>
+<li><a href="http:<?php echo $domain; ?>/projects/<?php echo $group_name;?>">Development version of <i>ouch</i> (on r-forge)</a></li>
+<li><a href="http://cran.at.r-project.org/web/packages/ouch/">Release version of <i>ouch</i> (on CRAN)</a></li>
+<li><a href="./index.php?nav=soft">Related software</a></li>
 </ul>
 </td>
 </tr>
 </table>
-
-<br>
-<br>
-<p>OUCH is software for phylogenetic comparative analysis. It is implemented in the <a href="http://www.r-project.org" target="_new">R language (www.r-project.org)</a> and can be downloaded from either the Comprehensive R Archive Network (<a href="http://cran.r-project.org/web/packages/ouch">cran.r-project.org</a>, stable version) or R-forge (<a href="http://ouch.r-forge.r-project.org">ouch.r-forge.r-project.org</a>, development version). </p>
-<p>The method is based on the ideas of Thomas F. Hansen (see
-T.&nbsp;F.&nbsp;Hansen, 1997. Stabilizing selection and the
-comparative analysis of adaptation. <em>Evolution</em>,
-<b>51</b>:1341-1351). It is explained fully in</p>
-<dl>
-<dd>Butler, M.A. and A.A. King, 2004. Phylogenetic comparative analysis: a modeling approach for adaptive evolution. <em>American Naturalist</em> <b>164</b>:683-695.</dd>
-</dl>
 <br>
 
-<p> You can find the <strong>project summary page</strong> <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-<p> The release version of the package is available on <a href="http://cran.at.r-project.org/web/packages/ouch/index.html">CRAN</a>.</p>
+<div align="left" width="80%">
+
+<?php
+$nav = $_REQUEST["nav"];
+switch ($nav) {
+    case "bib":
+        $dfile = "content/refs.htm";
+        break;
+    case "soft":
+        $dfile = "content/links.htm";
+        break;
+    default:
+        $dfile = "content/about.htm";
+        break;
+}
+include($dfile);
+?>
+
+</div>
+<hr width="100%">
 
 </body>
 </html>
+
