f53d21b4ba36dd76d134eef28617e73e15d16a41
kingaa <kingaa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-19 14:37:25 +0000

    - bug fix in webpage
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ouch@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -45,7 +45,7 @@ for phylogenetic <font color="#E32644">C</font>omparative hypot<font color="#E32
 <td>
 <ul>
 <li><a href="./index.php?nav=about">About <i>ouch</i></a></li>
-<li><a href="http:<?php echo $domain; ?>/projects/<?php echo $group_name;?>">Development version of <i>ouch</i> (on r-forge)</a></li>
+<li><a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name;?>">Development version of <i>ouch</i> (on r-forge)</a></li>
 <li><a href="http://cran.at.r-project.org/web/packages/ouch/">Release version of <i>ouch</i> (on CRAN)</a></li>
 <li><a target="_blank" href="http://cran.at.r-project.org/web/packages/ouch/ouch.pdf"><i>ouch</i> manual (PDF)</a></li>
 <li><a href="./index.php?nav=bib">References to the literature</a></li>
