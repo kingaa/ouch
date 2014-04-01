76caca61710dcfeadc670b95132df33adca6ff14
kingaa <kingaa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-01 08:05:16 +0000

    - revert to old version of glssoln.R
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ouch@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glssoln.R
+++ pkg/R/glssoln.R
@@ -4,29 +4,23 @@ glssoln <- function (a, x, v, tol = sqrt(.Machine$double.eps)) {
             chol(v),
             silent=FALSE
             )
-  if (inherits(vh,'try-error')) {
-    warning(
-            "glssoln: Choleski decomposition of variance-covariance matrix fails",
-            call.=FALSE
-            )
-    y <- rep(NA,ncol(a))
-    e <- rep(NA,n)
-    dim(y) <- ncol(a)
-    dim(e) <- n
-  } else {
-    s <- svd(forwardsolve(vh,a,upper.tri=TRUE,transpose=TRUE))
-    inds <- s$d > tol*max(s$d)
-    svals <- s$d[inds,drop=FALSE]
-    r <- length(svals)
-    svals <-  diag(1/svals,nrow=r,ncol=r)
-    y <- (s$v[,inds,drop=FALSE]%*%
-          (svals %*%
-           t(s$u[,inds,drop=FALSE])))%*%
-             forwardsolve(vh,x,upper.tri=TRUE,transpose=TRUE)
-    e <- a%*%y-x
-    dim(y) <- dim(y)[1]
-    dim(e) <- n
-  }
+  if (inherits(vh,'try-error'))
+    stop(
+         "error in Choleski decomposition of variance-covariance matrix",
+         call.=FALSE
+         )
+  s <- svd(forwardsolve(vh,a,upper.tri=TRUE,transpose=TRUE))
+  inds <- s$d > tol*max(s$d)
+  svals <- s$d[inds,drop=FALSE]
+  r <- length(svals)
+  svals <-  diag(1/svals,nrow=r,ncol=r)
+  y <- (s$v[,inds,drop=FALSE]%*%
+        (svals %*%
+         t(s$u[,inds,drop=FALSE])))%*%
+           forwardsolve(vh,x,upper.tri=TRUE,transpose=TRUE)
+  e <- a%*%y-x
+  dim(y) <- dim(y)[1]
+  dim(e) <- n
   list(
        coeff=y,
        residuals=e
