9ce6bcb2e066fbaf112bdfcab72c9e91b1c92c0f
Aaron A. King <kingaa@umich.edu>
2017-07-18 08:32:33 -0400

    register Callable routines; update package CITATION

---
+++ src/init.c
@@ -0,0 +1,17 @@
+#include "ouch.h"
+#include <R_ext/Rdynload.h>
+
+extern SEXP ouch_weights (SEXP object, SEXP lambda, SEXP S, SEXP beta);
+extern SEXP ouch_covar (SEXP object, SEXP lambda, SEXP S, SEXP sigmasq);
+
+static const R_CallMethodDef callMethods[] = {
+  {"ouch_weights", (DL_FUNC) &ouch_weights, 4},
+  {"ouch_covar", (DL_FUNC) &ouch_covar, 4},
+  {NULL, NULL, 0}
+};
+
+void R_init_ouch (DllInfo *info) {
+  // Register routines
+  R_registerRoutines(info,NULL,callMethods,NULL,NULL);
+  R_useDynamicSymbols(info,TRUE);
+}
