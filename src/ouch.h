ca463ba9910dc7dfe8c819b3a3245700abe023ed
Aaron A. King <kingaa@umich.edu>
2024-05-22 12:17:54 -0400

    remove inclusion of Rinternals.h

--- src/ouch.h
+++ src/ouch.h
@@ -4,14 +4,13 @@
 #include <R.h>
 #include <Rmath.h>
 #include <Rdefines.h>
-#include <Rinternals.h>
 
 static SEXP makearray (int rank, int *dim) {
   int nprotect = 0;
   int *dimp, k;
   SEXP dimx, x;
   PROTECT(dimx = NEW_INTEGER(rank)); nprotect++;
-  dimp = INTEGER(dimx); 
+  dimp = INTEGER(dimx);
   for (k = 0; k < rank; k++) dimp[k] = dim[k];
   PROTECT(x = allocArray(REALSXP,dimx)); nprotect++;
   UNPROTECT(nprotect);
